d9b3f8a607b789e8ce4a4d9325fe62705ad6ae88
ArthurScellesESILV <116368944+ArthurScellesESILV@users.noreply.github.com>
2023-03-03 13:33:12 +0100

    Add files via upload

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,37 +1,40 @@
 /* eslint-disable no-console, no-process-exit */
+
+
+
 const dedicatedbrand = require('./eshops/dedicatedbrand');
 const montlimart = require('./eshops/MontLimar');
-const circle = require('./eshops/Circle');
-const fs = require('fs');
+const circle = require('./eshops/Circle');  
+const fs = require('fs'); 
 
-async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
+async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/',eshop2 = 'https://www.montlimart.com/magasins#',eshop3 = 'https://shop.circlesportswear.com/collections/all') {
   try {
     console.log(`🕵️‍♀️  browsing ${eshop} eshop`);
 
     const products = await dedicatedbrand.scrape(eshop);
 
-    eshop2 = 'https://www.montlimart.com/101-t-shirts'
+    
     const products2 = await montlimart.scrape(eshop2);
 
-    eshop3 = 'https://shop.circlesportswear.com/collections/t-shirts-homme'
     const products3 = await circle.scrape(eshop3);
+
     let allProducts = products.concat(products2, products3);
-    const jsonData = JSON.stringify(products, null, 2);
+    const jsonData = JSON.stringify(allProducts, null, 2);
 
-
-
-
-    console.log(jsonData);
-    fs.writeFile('produit.json', jsonData, (err) => {
+    await fs.writeFile('produit.json', jsonData, (err) => {
       if (err) {
         throw err;
       }
       console.log('Les données ont été écrites dans le fichier produit.json.');
     });
+//writefilesinc https://nodejs.org/api/fs.html#fswritefilesyncfile-data-options
 
 
+
+    console.log(jsonData);
+    console.log(allProducts.length);
     console.log('done');
-    process.exit(0);
+    return products3;
 
   } catch (e) {
     console.error(e);
@@ -43,7 +46,6 @@ async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
 const [,, eshop] = process.argv;
 
 sandbox(eshop);
-
 
 
 
